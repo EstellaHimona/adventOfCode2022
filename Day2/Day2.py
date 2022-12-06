fb4380a5043471d92096a604addb1562829c70da
EstellaHimona <71775095+EstellaHimona@users.noreply.github.com>
2022-12-06 17:25:49 +0000

    Day2

---
+++ Day2/Day2.py
@@ -0,0 +1,19 @@
+#Rock - X - A - 1
+#Paper - Y - B - 2
+#Scissors - Z - C - 3
+
+#Lose - 0, Draw - 3, Win - 6
+
+def main():
+    f = open("input.txt", "r")
+    x = f.readlines()
+
+    
+
+
+if __name__ == "__main__":
+    t = main()
+    x = 0
+    for i in t:
+        x += i
+    print(x)    
