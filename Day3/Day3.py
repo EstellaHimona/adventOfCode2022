fddb293fb37f9f56cd9a4256d9806ece2354e255
EstellaHimona <71775095+EstellaHimona@users.noreply.github.com>
2022-12-07 12:53:17 +0000

    Day3

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -4,16 +4,32 @@
 #A-Z : 27-52
 #   ord (x) - ord ('A') + 27
 
-def main():
-    f = open("input.txt", "r")
-    x = f.readlines()
+# def main():
+from curses.ascii import isupper
 
+
+f = open("input.txt", "r")
+x = f.readlines()
+sum = 0
+
+for j in x:
+    firstHalf = j[:len(j)//2]
+    secondHalf = j[len(j)//2:]
     
+    for y in firstHalf:
+        if y in secondHalf:
+            break
+
+    if isupper(y):
+        sum += ord(y) - ord('A') + 27
+    else:
+        sum += ord(y) - ord('a') + 1
+print(sum)
 
 
-if __name__ == "__main__":
-    t = main()
-    x = 0
-    for i in t:
-        x += i
-    print(x)    
+# if __name__ == "__main__":
+#     t = main()
+#     x = 0
+#     for i in t:
+#         x += i
+#     print(x)    
